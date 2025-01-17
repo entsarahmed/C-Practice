de7c11744ea855eff3434cd28af55a18570da896
entsarahmed <entesara500@gmail.com>
2025-01-17 15:07:34 +0200

    Dice Game

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,37 +10,85 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            #region Calculator
 
-            int Num1;
-            int Num2;
-            string Answer;
-            int Result = 0;
-            Console.WriteLine("Please Enter Your First Number");
-            Num1= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please Enter Your Second Number");
-            Num2= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What type of operation would you like to do?");
-            Console.WriteLine("Please, Enter + for addition, - for subtraction, * for multiplication or any other key for division.");
-            Answer = Console.ReadLine();
-            if (Answer == "+")
-                Result = Num1 + Num2;
-            else if (Answer == "-")
-                Result = Num1 - Num2;
-            else if (Answer == "/")
-            {
-                if (Num2 == 0)
-                    Console.WriteLine("Please, you can't divide zero");
-                else
-                    Result = Num1 / Num2;
-            }
-            else
-            {
-                Result = Num1 * Num2;
-               
-            }
-            Console.WriteLine("The result is " + Result);
+            //    int Num1;
+            //    int Num2;
+            //    string Answer;
+            //    int Result = 0;
+            //    Console.WriteLine("Please Enter Your First Number");
+            //    Num1= Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Please Enter Your Second Number");
+            //    Num2= Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("What type of operation would you like to do?");
+            //    Console.WriteLine("Please, Enter + for addition, - for subtraction, * for multiplication or any other key for division.");
+            //    Answer = Console.ReadLine();
+            //    if (Answer == "+")
+            //        Result = Num1 + Num2;
+            //    else if (Answer == "-")
+            //        Result = Num1 - Num2;
+            //    else if (Answer == "/")
+            //    {
+            //        if (Num2 == 0)
+            //            Console.WriteLine("Please, you can't divide zero");
+            //        else
+            //            Result = Num1 / Num2;
+            //    }
+            //    else
+            //    {
+            //        Result = Num1 * Num2;
 
-        Console.ReadKey();
+            //    }
+            //    Console.WriteLine("The result is " + Result);
+
+            //Console.ReadKey(); 
+            #endregion
+
+            #region Dice Game
+            //int PlayerRandomNum;
+            //int EnemyRandomNum;
+            //int PlayerPoints = 0;
+            //int EnemyPoints = 0;
+
+            //Random random = new Random();
+            //for (int i = 0; i<10; i++)
+            //{
+            //    Console.WriteLine("Press Any Key to roll the dice.");
+            //    Console.ReadKey();
+            //    PlayerRandomNum = random.Next(1, 7);
+            //    Console.WriteLine("you rolled a" + PlayerRandomNum);
+
+            //    Console.WriteLine(".........");
+            //    System.Threading.Thread.Sleep(1000);
+
+            //    EnemyRandomNum = random.Next(1, 7);
+            //    Console.WriteLine("Enemy AI rolled a" +  EnemyRandomNum);
+
+            //    if (PlayerRandomNum > EnemyRandomNum)
+            //    {
+            //        PlayerPoints++;
+            //        Console.WriteLine("Player wins this round!");
+            //    }
+            //    else if (PlayerRandomNum < EnemyRandomNum)
+            //    {
+            //        EnemyPoints++;
+            //        Console.WriteLine("Enemy wins this round!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Draw!");
+            //    }
+            //    Console.WriteLine("The score is now - player : " + PlayerPoints + ". Enemy : " + EnemyPoints + ".");
+            //}
+            //if(PlayerPoints > EnemyPoints)
+            //    Console.WriteLine("You win!");
+            //else if( PlayerPoints < EnemyPoints)
+            //    Console.WriteLine("You lose!");
+            //else
+            //    Console.WriteLine("It's a draw!");
+            //Console.ReadKey();
+            #endregion
+
 
         }
     }
